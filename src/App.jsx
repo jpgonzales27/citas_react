095228fcd41f5111a976423e19edad2964b52ddc
Juan Pablo Gonzales <juampi7237@gmail.com>
2022-07-27 14:38:39 -0400

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { Formulario } from "./components/Formulario";
+import { Header } from "./components/Header";
+import { ListadoPacientes } from "./components/ListadoPacientes";
+
+function App() {
+  const [pacientes, setPacientes] = useState([]);
+  const [paciente, setPaciente] = useState({});
+
+  useEffect(() => {
+    const obtenerLs = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem("pacientes")) ?? [];
+      setPacientes(pacientesLS);
+    };
+
+    obtenerLs();
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("pacientes", JSON.stringify(pacientes));
+  }, [pacientes]);
+
+  const eliminarPaciente = (id) => {
+    console.log(`elimamnado paciente ${id}`);
+    const pacientesAtualizados = pacientes.filter(
+      (paciente) => paciente.id !== id
+    );
+    setPacientes(pacientesAtualizados);
+  };
+
+  return (
+    <div className="container mx-auto mt-20">
+      <Header />
+      <div className="mt-12 md:flex">
+        <Formulario
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
+        />
+        <ListadoPacientes
+          pacientes={pacientes}
+          setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
